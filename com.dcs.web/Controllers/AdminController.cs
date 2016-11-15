130f3f51646c3545a4007f2c01a405818b2e710a
Petrel <Petrel@DESKTOP-JOH39QL>
2016-11-16 00:12:18 +0800

    修改了Entity类DateTime的类型  添加了AddMember方法

--- com.dcs.web/Controllers/AdminController.cs
+++ com.dcs.web/Controllers/AdminController.cs
@@ -25,6 +25,8 @@ namespace com.dcs.web.Controllers
             return View();
         }
 
+        #region 页面加载时 需要调用到的方法
+        
         /// <summary>
         /// 获取当前登陆管理员下的所有员工 包括主管 收集员 普通员工
         /// </summary>
@@ -137,6 +139,57 @@ namespace com.dcs.web.Controllers
             return Json(ar, JsonRequestBehavior.AllowGet);
         }
 
+        #endregion
+
+        /// <summary>
+        /// 添加新用户
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult AddMember(MemberModel model)
+        {
+            AjaxResult ar = new AjaxResult();
+
+            if (model == null)
+            {
+                ar.state = ResultType.error;
+                ar.message = "提交的数据为空，添加失败";
+
+                return Json(ar,JsonRequestBehavior.AllowGet);
+            }
+
+            try
+            {
+                Member member = new Member();
+                var result = _memberBLL.AddMember(model.Name, model.RoleCode, model.ParentName, ref member);
+
+                if (result == OperatorState.error)
+                {
+                    ar.state = ResultType.error;
+                    ar.message = "添加新用户失败";
+                }
+                else if(result == OperatorState.success)
+                {
+                    ar.state = ResultType.success;
+                    ar.data = member;
+                    ar.message = "添加成功";
+                }
+
+                return Json(ar, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception ex)
+            {
+                LogHelper.writeLog_error(ex.Message);
+                LogHelper.writeLog_error(ex.StackTrace);
+
+                ar.state = ResultType.error;
+                ar.message = "系统错误，添加新用户失败";
+
+                return Json(ar, JsonRequestBehavior.AllowGet);
+            }
+        }
         private List<MemberModel> ChangeTOMemberModel(IEnumerable<Member> memberList)
         {
             List<MemberModel> mmList = new List<Models.MemberModel>();
